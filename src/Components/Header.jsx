00c86e43ca2bb961095bfdacd719d77338d22567
LordMashh <mayankkush0842@gmail.com>
2023-08-16 18:59:39 +0530

    adjustment commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-slate-600 text-white py-4">
       <div className="container mx-auto flex justify-center items-center">
-        <h1 className="text-3xl font-semibold">DELIGHTFUL RECIPE FINDER</h1>
+        <h1 className="text-xl sm:text-3xl  font-semibold">DELIGHTFUL RECIPE FINDER</h1>
       </div>
     </header>
   );
